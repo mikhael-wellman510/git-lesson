1292405c3e7ff70be23fe307a31aa101664fd6ee
Mikhael-Wellman <mikhaelwellman423@gmail.com>
2025-09-04 14:49:10 +0700

    second-commit

---
+++ src/main/java/com/example/sharding_db/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.example.sharding_db.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	public void test1(){
+	}
+}
